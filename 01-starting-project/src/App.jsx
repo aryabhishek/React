7bda01b8ff26fba9f015cb947b8c1fd7c119dbd6
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-02-06 02:13:28 +0530

    learned useState

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,26 +1,44 @@
-import { CORE_CONCEPTS } from "./data";
-
-function CoreConcept({ title, image, desc }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{desc}</p>
-    </li>
-  );
-}
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import { useState } from "react";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
+  const [activeTab, setActiveTab] = useState();
+  function handleSelect(selectedButton) {
+    setActiveTab(selectedButton);
+  }
+
   return (
     <div id="app" data-testid="app">
       <Header />
       <main>
         <section id="core-concepts">
-          <ul><CoreConcept {...CORE_CONCEPTS[0]} />
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={activeTab === "components"} onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton isSelected={activeTab === "jsx"} onSelect={() => handleSelect("jsx")}>Jsx</TabButton>
+            <TabButton isSelected={activeTab === "props"} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={activeTab === "state"} onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {!activeTab && <p>Please click on a tab!</p>}
+          {activeTab && <div id="tab-content">
+            <h3>{EXAMPLES[activeTab].title}</h3>
+            <p>{EXAMPLES[activeTab].description}</p>
+            <pre>
+              <code>{EXAMPLES[activeTab].code}</code>
+            </pre>
+          </div>}
         </section>
       </main>
 
--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -1,13 +1,15 @@
+import reactImg from "../assets/react-core-concepts.png";
+
 function Header() {
     return (
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are going to build!
-        </p>
-      </header>
+        <header>
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                Fundamental React concepts you will need for almost any app you are going to build!
+            </p>
+        </header>
     );
-  }
+}
 
-  export default Header;
+export default Header;
