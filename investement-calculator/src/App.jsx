fd2f4f2b3fe78e043b44175058c4849044845b27
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-02-07 21:22:58 +0530

    completed investement-calculator

--- investement-calculator/src/App.jsx
+++ investement-calculator/src/App.jsx
@@ -1,7 +1,33 @@
+import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const isValidDuration = userInput.duration > 0;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIdentifier]: +newValue };
+    });
+    console.log(`${inputIdentifier} changed to ${newValue}`);
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header></Header>
+      <UserInput userInput={userInput} onChange={handleChange}></UserInput>
+      {!isValidDuration && <p className="center">Please Enter a valid duration!</p>}
+      {isValidDuration && <Results input={userInput}></Results>}
+    </>
+  );
 }
 
-export default App
+export default App;
