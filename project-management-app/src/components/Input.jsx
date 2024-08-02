1e22481ed86d9bebd1c19af18d8e1459ccb22a2c
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-08-03 01:36:32 +0530

    add a bunch of components

---
+++ project-management-app/src/components/Input.jsx
@@ -0,0 +1,12 @@
+export default function Input({ label, textArea }) {
+  const classes =
+    "w-full p-1 border-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+  return (
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textArea ? <textarea className={classes}></textarea> : <input className={classes}></input>}
+    </p>
+  );
+}
