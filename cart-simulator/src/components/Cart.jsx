57ded79989f8b23afcfcb8c728140440a0f38faf
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-08-06 20:24:21 +0530

    solve prop drilling

--- cart-simulator/src/components/Cart.jsx
+++ cart-simulator/src/components/Cart.jsx
@@ -1,4 +1,8 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Cart() {
+  const { items, updateCart } = useContext(CartContext);
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -20,13 +24,9 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
-                    -
-                  </button>
+                  <button onClick={() => updateCart(item.id, -1)}>-</button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
-                    +
-                  </button>
+                  <button onClick={() => updateCart(item.id, 1)}>+</button>
                 </div>
               </li>
             );
