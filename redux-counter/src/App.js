9553a195650b16b8e82d1dd08f22c420be98a6b8
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-08-24 19:19:55 +0530

    make store, slices and finish up

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,9 +1,21 @@
-import Counter from './components/Counter';
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
 
+import Counter from "./components/Counter.js";
+import Header from "./components/Header.js";
+import Auth from "./components/Auth.js";
+import UserProfile from "./components/UserProfile.js";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
