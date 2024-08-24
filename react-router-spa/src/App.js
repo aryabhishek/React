4e9e9a9ef5ed5a278dd99854f5ac2e8d5d0cd17c
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-08-24 20:00:53 +0530

    create components & add pages fix bugs finish up

--- react-router-spa/src/App.js
+++ react-router-spa/src/App.js
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home.js";
+import ProductsPage from "./pages/Products.js";
+import RootLayout from "./pages/Root.js";
+import ErrorPage from "./pages/Error.js";
+import ProductDetailPage from "./pages/ProductDetail.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> }, // path: ""
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
