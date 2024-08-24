9cedd609b4eb8f513b426ef153694e12b2f4e1bd
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-08-24 20:52:37 +0530

    dynamic queries and mutations

--- react-events-hub/src/components/Events/EditEvent.jsx
+++ react-events-hub/src/components/Events/EditEvent.jsx
@@ -1,27 +1,95 @@
-import { Link, useNavigate } from 'react-router-dom';
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useParams,
+  useSubmit,
+  useNavigation,
+} from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { fetchEvent, queryClient, updateEvent } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
 
-  function handleSubmit(formData) {}
+  const submit = useSubmit();
+  const params = useParams();
 
-  function handleClose() {
-    navigate('../');
+  const { data, isError, error } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    staleTime: 10000,
+  });
+
+  function handleSubmit(formData) {
+    submit(formData, { method: "PUT" });
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+  function handleClose() {
+    navigate("../");
+  }
+
+  let content;
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="Failed to load event"
+          message={
+            error.info?.message ||
+            "Failed to load event. Please check your inputs and try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
+        {state === "submitting" ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updateEventData = Object.fromEntries(formData);
+
+  await updateEvent({ id: params.id, event: updateEventData });
+  await queryClient.invalidateQueries(["events"]);
+
+  return redirect("../");
 }
